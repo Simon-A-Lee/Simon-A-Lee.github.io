1cbd183a1e9c908fa808be19499f7a8603c6bc5c
Simonlee711 <simon.lee711@gmail.com>
2025-01-17 23:16:17 +0000

    Deploying to gh-pages from @ Simon-A-Lee/Simon-A-Lee.github.io@f376c6471a82d73eb7be3daba1756b67fb4b4372 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%69%6D%6F%6E%6C%65%65%37%31%31@%67.%75%63%6C%61.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Simonlee711", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -500,6 +507,27 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/simonlee.a", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/simon-a-lee-b71525319", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-3324-9772", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,7 +541,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=rdQAAAAJ&hl", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/SimonLee79475", "_blank");
+        },
+      },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@youtube.com/@simonlee.a", "_blank");
         },
       },{
         id: 'social-custom_social',
